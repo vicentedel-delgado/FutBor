cb973badd66441243a6980c46c09de77397321b1
vicentedel-delgado <141055191+vicentedel-delgado@users.noreply.github.com>
2023-09-30 14:08:38 -0500

    modificando 4 paginas

--- ADMINCANCHAS/LaDelis/conexion.php
+++ ADMINCANCHAS/LaDelis/conexion.php
@@ -1,14 +1,14 @@
 <?php
-$servername = "localhost"; // Nombre del servidor (puede variar)
-$username = "FutBor"; // Nombre de usuario de la base de datos
-$password = "-COco22-"; // Contraseña de la base de datos
-$dbname = "ladelis"; // Nombre de la base de datos
+$servername = "localhost";
+$username = "FutBor";
+$password = "-COco22-";
+$dbname = "ladelis";
 
 // Crear la conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Verificar la conexión
 if ($conn->connect_error) {
-  die("Conexión fallida: " . $conn->connect_error);
+    die("Error de conexión: " . $conn->connect_error);
 }
 ?>
