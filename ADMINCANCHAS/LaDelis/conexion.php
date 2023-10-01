37e0c3ef53940a0f46647342c7ff2b2647dcdab7
vicentedel-delgado <141055191+vicentedel-delgado@users.noreply.github.com>
2023-09-30 19:59:48 -0500

    hellos

--- ADMINCANCHAS/LaDelis/conexion.php
+++ ADMINCANCHAS/LaDelis/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "127.0.0.1";  // Comillas dobles
+$servername = "Apache/2.4.56 (Win64) OpenSSL/1.1.1t PHP/8.2.4";  // Comillas dobles
 $username = 'FutBor';  // Comillas simples
 $password = '-COco22-';  // Comillas simples
 $dbname = 'ladelis';  // Comillas simples
